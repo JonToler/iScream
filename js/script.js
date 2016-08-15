439746507979a0a2eb7e9310e339dd133f90059a
HK Kahng and Jonathan Toler <hk@>
2016-08-15 13:06:04 -0700

    add code to alpha sort and capitalize list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   $("form#youScream").submit(function(event) {
     var fieldNames = [1,2,3,4];
+    var sortedList = [];
 
     fieldNames.forEach(function(fieldName) {
-      var flavor = "<li>" + $("#flavor" + fieldName).val() + "</li>";
+      sortedList.push($("#flavor" + fieldName).val().toUpperCase());
+    });
+    sortedList.sort();
+
+    sortedList.forEach(function(sortedItem) {
+      var flavor = "<li>" + sortedItem + "</li>";
       $("#flavorFavs").append(flavor);
-    })
+    });
 
     event.preventDefault();
   });
